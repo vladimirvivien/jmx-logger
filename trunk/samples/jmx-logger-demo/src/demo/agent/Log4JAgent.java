377f7d73c7b893834da5e740eaed89ca0668a760
vladimir.vivien <vladimir.vivien@52d5289e-0e2d-11de-b8f3-b9fb9eb71b04>
2009-10-19 11:09:50 +0000

    Testing new log4j jmx-logger features.

--- trunk/samples/jmx-logger-demo/src/demo/agent/Log4JAgent.java
+++ trunk/samples/jmx-logger-demo/src/demo/agent/Log4JAgent.java
@@ -12,24 +12,24 @@ import org.apache.log4j.Logger;
  * @author vladimir
  */
 public class Log4JAgent {
-    private static final Map<Level,String> logs =  new HashMap<Level,String>();
-    static {
-        logs.put(Level.INFO, "I am happy!");
-        logs.put(Level.WARN, "I am concerned...");
-        logs.put(Level.ERROR, "I am in trouble, something went wrong.");
-        logs.put(Level.DEBUG, "I am up, I am down, I am all around!");
-    }
-
-    public static void main(String[] args) throws InterruptedException{
-        Logger logger = Logger.getLogger(LoggingUtilAgent.class);
-
-        int next = 0;
+    private static Logger logger = Logger.getLogger(Log4JAgent.class);
+    private static String[] messages = {
+        "I am happy!",
+        "I am concerned...",
+        "I am in trouble, something went wrong.",
+        "I am up, I am down, I am all around!"
+    };
+    private static Level[] levels = {Level.INFO, Level.WARN, Level.ERROR, Level.DEBUG, Level.TRACE, Level.FATAL};
+    public static void main (String[] args) {
         for(;;){
-            Map.Entry<Level,String> entry = (Entry<Level, String>) logs.entrySet().toArray()[next];
-            logger.log(entry.getKey(), entry.getValue());
-            next = new Random().nextInt(logs.size());
-            Thread.currentThread().sleep(2000);
+            int nextMsg = new Random().nextInt(4);
+            int nextLevel = new Random().nextInt(6);
+            logger.log(levels[nextLevel], messages[nextMsg]);
+            try {
+                Thread.currentThread().sleep(700);
+            } catch (InterruptedException ex) {
+                java.util.logging.Logger.getLogger(Log4JAgent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
         }
-
     }
 }
