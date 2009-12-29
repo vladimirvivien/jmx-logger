455f0e267a714bc614f6fd864a2e326016731783
vladimir.vivien <vladimir.vivien@52d5289e-0e2d-11de-b8f3-b9fb9eb71b04>
2009-12-29 12:11:23 +0000

    Buiding a logger console.

--- src/jmxlogger/tools/loghub/Main.java
+++ src/jmxlogger/tools/loghub/Main.java
@@ -39,10 +39,10 @@ public class Main extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("LogHub");
+        setTitle("LogConsole");
         setMinimumSize(new java.awt.Dimension(640, 480));
         setName("mainFrame"); // NOI18N
-        setResizable(false);
+        setPreferredSize(new java.awt.Dimension(640, 480));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
