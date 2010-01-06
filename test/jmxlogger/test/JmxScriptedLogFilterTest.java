1f34ecd8ad2cc869caa4edeac1b564acb62425c5
vladimir.vivien <vladimir.vivien@52d5289e-0e2d-11de-b8f3-b9fb9eb71b04>
2010-01-06 21:43:12 +0000

    Finalizing expression engine setup.

--- test/jmxlogger/test/JmxScriptedLogFilterTest.java
+++ test/jmxlogger/test/JmxScriptedLogFilterTest.java
@@ -45,74 +45,69 @@ public class JmxScriptedLogFilterTest {
     @Test
     public void testIsLogAllowedWithExpression() {
         event.put(ToolBox.KEY_EVENT_LEVEL, "INFO");
-        JmxEventWrapper eventwrap = new JmxEventWrapper(event);
         // if test is absent, return true
-         assert filter.isLogAllowed(eventwrap);
+         assert filter.isLogAllowed(event);
          filter.setFilterExpression("logLevel == 'INFO'");
-         assert filter.isLogAllowed(eventwrap);
+         assert filter.isLogAllowed(event);
     }
 
     @Test
     public void testIsLogAllowedWithBlankExpression() {
         event.put(ToolBox.KEY_EVENT_LEVEL, "INFO");
-        JmxEventWrapper eventwrap = new JmxEventWrapper(event);
         // if test is absent, return true
-         assert filter.isLogAllowed(eventwrap);
+         assert filter.isLogAllowed(event);
          filter.setFilterExpression("");
-         assert filter.isLogAllowed(eventwrap);
+         assert filter.isLogAllowed(event);
     }
 
     @Test
     public void testIsLogAllowedWithScriptFile() {
         event.put(ToolBox.KEY_EVENT_LEVEL, "INFO");
-        JmxEventWrapper eventwrap = new JmxEventWrapper(event);
-
         try{
             filter.setScriptFile(new File("bad-file.mvl"));
             throw new IllegalStateException("Test should have filed upon missing file.");
         }catch(Exception ex){}
 
         filter.setScriptFile(new File ("test-script.mvl"));
-        assert filter.isLogAllowed(eventwrap);
+        assert filter.isLogAllowed(event);
     }
 
     @Test
     public void testFormattedMessage() {
         event.put(ToolBox.KEY_EVENT_FORMATTED_MESSAGE, debugMsg);
-        JmxEventWrapper eventwrap = new JmxEventWrapper(event);
         filter.setFilterExpression("formattedMessage contains 'DEBUG'");
-        assert filter.isLogAllowed(eventwrap);
+        assert filter.isLogAllowed(event);
 
         filter.setFilterExpression("formattedMessage contains 'INFO'");
-        assert ! filter.isLogAllowed(eventwrap);
+        assert ! filter.isLogAllowed(event);
     }
 
     @Test
     public void testRawMessage() {
         event.put(ToolBox.KEY_EVENT_RAW_MESSAGE, "ERROR, STOP EVERYTHING");
-        JmxEventWrapper eventwrap = new JmxEventWrapper(event);
+
         filter.setFilterExpression("rawMessage.length() == 22");
-        assert filter.isLogAllowed(eventwrap);
+        assert filter.isLogAllowed(event);
     }
 
     @Test
     public void testSourceClass() {
         event.put(ToolBox.KEY_EVENT_SOURCE_CLASS, this.getClass().getName());
-        JmxEventWrapper warpper = new JmxEventWrapper(event);
+
         filter.setFilterExpression(String.format("sourceClassName == '%s'", this.getClass().getName()));
-        assert filter.isLogAllowed(warpper);
+        assert filter.isLogAllowed(event);
         filter.setFilterExpression("sourceClassName == 'jxmlogger.tools.JmxLogFilter'");
-        assert !filter.isLogAllowed(warpper);
+        assert !filter.isLogAllowed(event);
     }
 
     @Test
     public void testSourceMethod() {
         event.put(ToolBox.KEY_EVENT_SOURCE_METHOD, "isLogAllowed");
-        JmxEventWrapper warpper = new JmxEventWrapper(event);
+
         filter.setFilterExpression("sourceMethodName == 'isLogAllowed'");
-        assert filter.isLogAllowed(warpper);
+        assert filter.isLogAllowed(event);
         filter.setFilterExpression("sourceMethodName == 'testSourceMethodName'");
-        assert !filter.isLogAllowed(warpper);
+        assert !filter.isLogAllowed(event);
     }
 
     @Test
@@ -120,14 +115,14 @@ public class JmxScriptedLogFilterTest {
         event.put(ToolBox.KEY_EVENT_THROWABLE, "java.lang.RuntimeException");
         JmxEventWrapper warpper = new JmxEventWrapper(event);
         filter.setFilterExpression("exceptionName == 'java.lang.RuntimeException'");
-        assert filter.isLogAllowed(warpper);
+        assert filter.isLogAllowed(event);
 
         filter.setFilterExpression("exceptionName.equals('java.lang.Exception')");
-        assert !filter.isLogAllowed(warpper);
+        assert !filter.isLogAllowed(event);
     }
 
 
-@Test
+    @Test
     public void testTimeStamp() {
         long today = new Date().getTime();
         Calendar cal = Calendar.getInstance();
@@ -136,10 +131,35 @@ public class JmxScriptedLogFilterTest {
         long tomorrow = cal.getTimeInMillis();
         event.put(ToolBox.KEY_EVENT_TIME_STAMP, tomorrow);
 
-        JmxEventWrapper warpper = new JmxEventWrapper(event);
         filter.setFilterExpression("timestamp > new Date().getTime()");
-        assert filter.isLogAllowed(warpper);
+        assert filter.isLogAllowed(event);
         filter.setFilterExpression("timestamp < new Date().getTime()");
-        assert !filter.isLogAllowed(warpper);
+        assert !filter.isLogAllowed(event);
+    }
+
+    @Test
+    public void testStatistics() {
+        Map<String, Long> stats = new HashMap<String,Long>();
+        stats.put(ToolBox.KEY_EVENT_START_TIME, new Long(new Date().getTime()));
+        stats.put(ToolBox.KEY_EVENT_LOG_COUNTED, new Long(12345678));
+        stats.put(ToolBox.KEY_EVENT_LOG_COUNT_ATTEMPTED, new Long(87654321));
+        stats.put("INFO", new Long(12345));
+        stats.put("DEBUG", new Long(1234567));
+        event.put(ToolBox.KEY_EVENT_LOG_STAT, stats);
+
+        filter.setFilterExpression("logStats['startTime'] < (new Date().getTime() + 10000)");
+        assert filter.isLogAllowed(event) : "Unable to evaluate stats from event map";
+
+        filter.setFilterExpression("logStats.totalLogCounted == new Long(12345678)");
+        assert filter.isLogAllowed(event) : "Unable to evaluate stats from event map";
+
+        filter.setFilterExpression("logStats.totalLogCounted < logStats.totalLogAttempted");
+        assert filter.isLogAllowed(event) : "Unable to evaluate stats from event map";
+
+        filter.setFilterExpression("logStats.INFO == new Long(12345)");
+        assert filter.isLogAllowed(event) : "Unable to evaluate stats from event map";
+
+        filter.setFilterExpression("logStats.DEBUG > logStats.INFO");
+        assert filter.isLogAllowed(event) : "Unable to evaluate stats from event map";
     }
 }
